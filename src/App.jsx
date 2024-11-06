73aca936e8fd41e33813ec419c4d6f15311326b5
LeonathaTurtle <mancillh@gmail.com>
2024-11-06 10:29:26 -0600

    added facebook logo, rewrote footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 // Custom Styles
 import './styles/App.css'
 import { Outlet } from 'react-router-dom';
-// import Footer from "./components/Footer";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 
 export default function App() {
@@ -11,7 +11,7 @@ export default function App() {
     <>
       <Header />
       <Outlet />
-      {/* <Footer /> */}
+      <Footer />
     </>
   )
 }
--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,13 +1,17 @@
+// Styles for Footer
 import '../styles/Footer.css';
 
+// Renders Footer Component
 export default function Footer() {
     return (
         <>
         <footer>
             <h6>Made with &#10084; by Hannah Mancill</h6>
+            <div className="footer-links">
+                <a href='https://www.facebook.com/profile.php?id=61568116504234'><img src='/images/Facebook_Logo_Primary.png' alt='Facebook logo'></img></a>
+            </div>
         </footer>
         </>
-    )
+    );
 }
-
  
