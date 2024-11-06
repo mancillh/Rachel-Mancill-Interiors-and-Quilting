9365c395ea70af9a9936c4ca72a3077b20ad04ba
LeonathaTurtle <mancillh@gmail.com>
2024-11-06 10:43:18 -0600

    rewrote Footer import at top of App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 // Component Imports
 import { Outlet } from 'react-router-dom';
-import Footer from "./components/Footer";
+import Footer from './components/Footer.jsx';
 import Header from "./components/Header";
 
 // Bootstrap Styles
