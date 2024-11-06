40a781182ddb08001e9df6788d11946a57500b08
LeonathaTurtle <mancillh@gmail.com>
2024-11-05 20:13:22 -0600

    added empty html tags to help footer render

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,10 +2,12 @@ import '../styles/Footer.css';
 
 export default function Footer() {
     return (
+        <>
         <footer>
             <h6>Made with &#10084; by Hannah Mancill</h6>
         </footer>
-    );
+        </>
+    )
 }
 
  
