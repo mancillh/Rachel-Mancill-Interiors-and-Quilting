49bd2fe17f5c908789c42df43a3164064c2279ea
LeonathaTurtle <mancillh@gmail.com>
2024-11-06 10:35:59 -0600

    removed footer semantic html tag

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -5,12 +5,12 @@ import '../styles/Footer.css';
 export default function Footer() {
     return (
         <>
-        <footer>
+        <div>
             <h6>Made with &#10084; by Hannah Mancill</h6>
             <div className="footer-links">
                 <a href='https://www.facebook.com/profile.php?id=61568116504234'><img src='/images/Facebook_Logo_Primary.png' alt='Facebook logo'></img></a>
             </div>
-        </footer>
+        </div>
         </>
     );
 }
