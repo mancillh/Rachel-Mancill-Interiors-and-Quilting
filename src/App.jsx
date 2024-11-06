f0c6ecaa380d68d61205322bd408e358e1533ce1
LeonathaTurtle <mancillh@gmail.com>
2024-11-06 10:48:25 -0600

    rewrote App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 // Component Imports
 import { Outlet } from 'react-router-dom';
-import Footer from './components/Footer.jsx';
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 
 // Bootstrap Styles
@@ -8,7 +8,9 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 // Custom Styles
 import './styles/App.css'
 
-export default function App() {
+//Renders Header, Outlet, and Footer on each "page" of the website. The Outlet is a placeholder within a 
+//parent route's component to render the matching child routes.
+function App() {
 
   return (
     <>
@@ -18,3 +20,6 @@ export default function App() {
     </>
   )
 }
+
+export default App
+
