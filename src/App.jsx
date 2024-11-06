cb98bc64eda7927331b0db22f648f343480eefd4
LeonathaTurtle <mancillh@gmail.com>
2024-11-06 10:33:21 -0600

    troubleshooting footer deploy

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 // Custom Styles
 import './styles/App.css'
 import { Outlet } from 'react-router-dom';
-import Footer from "./components/Footer";
+import Footer from './components/Footer.jsx';
 import Header from "./components/Header";
 
 export default function App() {
