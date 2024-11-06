5fda42e5353ffcc8b0ce0e078eb26e97c81aac20
LeonathaTurtle <mancillh@gmail.com>
2024-11-06 14:03:02 -0600

    edited about us section

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -4,11 +4,6 @@ import '../styles/Footer.css';
 // Renders Footer Component
 export default function Footer() {
     return (
-        <div>
-            <div className="footer-links">
-                <a href='https://www.facebook.com/profile.php?id=61568116504234'><img src='/images/Facebook_Logo_Primary.png' alt='Facebook logo'></img></a>
-            </div>
-            <h6>Made with &#10084; by Hannah Mancill</h6>
-        </div>
+        <h6>Made with &#10084; by Hannah Mancill</h6>
     );
 }
--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -13,12 +13,21 @@ export default function AboutPage() {
             <Container>
                 <Row>
                     <Col>
-                        <p className="about">Rachel has been quilting for decades. She particularly enjoys
-                            using vintage linens and wild and vibrant colors in her work. Rachel is the
-                            designer and piecer of quilt tops. Her husband, Mike, is the long-armer and
-                            specializes in custom designs. Our studio is located in Katy, TX and we offer
-                            bespoke quilting services of various kinds. Contact us with the form below for
-                            more information. Quotes are free.</p>
+                        <p className="about">Welcome! I’m Rachel Mancill, an interior designer and longarm quilter with a passion for
+                            creating spaces and textiles that feel as beautiful as they are personal. My journey began with a love for
+                            color, texture, and craftsmanship, and over the years, I’ve developed a unique approach that blends these two
+                            fields.</p>
+
+                        <p className="about">In interior design, I believe that every space tells a story. My goal is to help you define
+                            yours, curating environments that reflect your style and personality while balancing functionality and beauty. Since I am a quilter I have special insight into the organizational challenges of  storing fabric and thread stashes as well as tools. I understand how important ease of access is. Whether it’s designing a cozy living room, a productive workspace, or a serene bedroom, I strive to bring out the best in every area of your home.
+                        </p>
+
+                        <p className="about">My quilting is inspired by everything from traditional patterns to modern aesthetics. For the
+                            convenience of our longarm customers we stock a nice selection of mostly neutral widebacks including Moda Grunge,
+                            Ruby Star Society, and Michael Miller. </p>
+
+                        <p className="about">Let’s work together to bring your vision to life—whether it’s through thoughtful interior design
+                            or a one-of-a-kind quilt. I can’t wait to help you create something special.</p>
                     </Col>
                 </Row>
                 <Row className="aboutPhotoContainer">
@@ -30,7 +39,12 @@ export default function AboutPage() {
                     </Col>
                 </Row>
                 <Row className='follow'>Follow Rachel Mancill Interiors & Quilting</Row>
-            </Container>
+                <Row>
+                    <div className="facebook-link">
+                        <a href='https://www.facebook.com/profile.php?id=61568116504234'><img src='/images/Facebook_Logo_Primary.png' alt='Facebook logo'></img></a>
+                    </div>
+                </Row>
+            </Container >
         </>
     )
 }
