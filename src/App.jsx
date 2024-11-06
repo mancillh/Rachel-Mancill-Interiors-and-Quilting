d018b8a966f6042d058006a0a7fb6762b177f179
LeonathaTurtle <mancillh@gmail.com>
2024-11-06 10:37:53 -0600

    rewrote imports for app.jsx troubleshooting footer deply

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+// Component Imports
+import { Outlet } from 'react-router-dom';
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
+// Bootstrap Styles
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 // Custom Styles
 import './styles/App.css'
-import { Outlet } from 'react-router-dom';
-import Footer from './components/Footer.jsx';
-import Header from "./components/Header";
 
 export default function App() {
 
